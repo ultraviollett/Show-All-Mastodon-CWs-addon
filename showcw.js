fb09fab2189c8c4abf978798de04df78b645536b
ALM <86886983+ultraviollett@users.noreply.github.com>
2024-01-30 21:23:17 +0000

    Add ability (in code) to toggle show and hide

--- showcw.js
+++ showcw.js
@@ -1,39 +1,57 @@
-prt = x => console.log(x);
+prt = (x) => console.log(x);
 
-function showCw(node){
-const post = node.getElementsByClassName("status__content__text");
+function toggleCw(node, show = true) {
+  /*
+    function to toggle between showing and hiding the CW text in a post.
+    show = true -> showing the posts
+    show = false -> hiding the posts
+    */
+  const classToFind = show ? "status__content__text" : "show-cw-addon";
+  const postList = node.getElementsByClassName(classToFind);
 
-for (let i = 0; i < post.length; i++){
-    post[i].classList.add( "status__content__text--visible show-cw-addon" );
-};
+  if (show) {
+    //if you are showing posts, 
+    for (const post of postList) {
+    if (!post.classList.contains("status__content__text--visible")){
+      post.classList.add("status__content__text--visible");
+      post.classList.add("show-cw-addon");
+    }
+    }
+  } else {
+    for (const post of postList) {
+      post[i].classList.remove("status__content__text--visible");
+    }
+  }
 
-prt(`${post.length} CW posts shown!`)
+  const buttonList = node.getElementsByClassName(
+    "status__content__spoiler-link"
+  );
+
+  for (const btn of buttonList) {
+    //change name of button depending on if you show or hide
+    btn.textContent = show ? "Showing All" : "Hiding All";
+    btn.disabled = "true";
+  }
+
+  prt( `${postList.length} CW posts ${show ? "shown" : "hidden"}`);
 }
 
-function hideCw(node){
-const post = node.getElementsByClassName("show-cw-addon");
-
-for (let i = 0; i < post.length; i++){
-    post[i].classList.remove( "status__content__text--visible show-cw-addon" );
-};
-prt(`${post.length} CW posts hidden!`)
-}
-
-showCw(document);
+toggleCw(document);
 
 const observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      if (mutation.addedNodes && mutation.addedNodes.length > 0) {
-        // This DOM change was new nodes being added. Run our substitution
-        // algorithm on each newly added node.
-        for (let i = 0; i < mutation.addedNodes.length; i++) {
-          const newNode = mutation.addedNodes[i];
-          showCw(newNode);
-        }
+  mutations.forEach((mutation) => {
+    if (mutation.addedNodes && mutation.addedNodes.length > 0) {
+      // This DOM change was new nodes being added. Run our substitution
+      // algorithm on each newly added node.
+      for (let i = 0; i < mutation.addedNodes.length; i++) {
+        const newNode = mutation.addedNodes[i];
+        toggleCw(newNode);
       }
-    });
+    }
   });
-  observer.observe(document.body, {
-    childList: true,
-    subtree: true,
-  });
+});
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
+
